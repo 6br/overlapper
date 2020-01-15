65cfc13f07c49833bbff1e57c56dc55d2417b432
test <test@noreply>
2020-01-15 15:13:41 +0900

    Update codes

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,28 @@ module Main where
 import Lib
 import Bio.Paf
 import Bio.Paf.IO
+import qualified Data.Vector as V
+import Data.List
+import Data.Graph
+
+compareByQName :: Aln -> Aln -> Bool
+compareByQName a b = (_qname a) == (_qname b)
+
 
 main :: IO ()
 main = do
     paf <- readPafFile "test/data/test.paf"
-    ovlp <- pafToOvlp paf
+    let grouped = filter (\x -> (length x) >= 2) $groupBy compareByQName $ filter (\x -> _mapq x > 0) $ V.toList $ _alns paf 
+    print $ grouped
+    let ovlp = pafToOvlp paf
     print $ take 5 ovlp
-    (graph, nodeFromVertex, vertexFromKey) = graphFromEdges edgeList
-    print nodeFromVertex
-    print vertexFromkey
+    let edges = map ctgPairToEdge ovlp
+    let graph = buildG (0, maximum $ concat $ map pairToList edges) $ edges
     print graph
-  
+    --let (graph, nodeFromVertex, vertexFromKey) = graphFromEdges ovlp
+    --print nodeFromVertex
+    --print vertexFromKey
+    --print graph
+
+pairToList :: (a, a) -> [a]
+pairToList (x,y) = [x,y]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,65 +1,85 @@
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
 module Lib
-    ( pafToOvlp
+    ( pafToOvlp,
+      ctgPairToEdge
     ) where
 
 import Bio.Paf
 import Bio.Paf.IO
+import Data.Char
 import Data.Graph
+import Data.List
 import qualified Data.Text as T
+import Data.Text.Read 
+import qualified Data.Vector as V
 import GHC.Generics
+import GHC.Int
 
 data Contig = Contig {
    contigId :: Int,
-   strand :: Bool
-} deriving (Generic, Show)
+   posStrand :: Bool
+} deriving (Show)
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+contigAsInt :: Contig -> Int
+contigAsInt a = case (posStrand a) of
+  True -> (contigId a) * 2
+  False -> (contigId a) * 2 + 1 
 
-hangLength :: Int
+hangLength :: Int32
 hangLength = 1000
 
-adjacentLength :: Int
+adjacentLength :: Int32
 adjacentLength = 10000
 
+minMapQ :: Int32
+minMapQ = 0
+
 compareByQName :: Aln -> Aln -> Bool
-compareByQName a b = (_qName a) == (_qname b)
+compareByQName a b = (_qname a) == (_qname b)
 
 isHangingAlignment:: Aln -> Bool
 isHangingAlignment aln = case (_strand aln) of
     '+' -> (_tlen aln - hangLength) <= (_tend aln)
-    '-' -> (_tstart) <= hangLength
+    '-' -> (_tstart aln) <= hangLength
 
 isOverlapAlignment :: Aln -> Aln -> Bool
 isOverlapAlignment a b = isHangingAlignment a && isHangingAlignment b && (_qstart b - _qend a) <= adjacentLength
 
 extractOverlapAlignment :: [Aln] -> [(Contig, Contig)]
-extractOverlapAlignment aln = map convertToEdge $ filter isOverlapAlignment $ eachCons aln
+extractOverlapAlignment aln = map (uncurry convertToEdge) $ filter (uncurry isOverlapAlignment) $ map (\x -> (x !! 0, x !! 1)) $ eachCons 2 aln
 
-pafToOvlp :: Paf -> [Contig]
+pafToOvlp :: Paf -> [(Contig, Contig)]
 pafToOvlp paf = do
-    let grouped = groupBy compareByQName $ filter (\x -> _mapq x > 0) paf 
-    let edgeList = map (extractOverlapAlignment) grouped 
-    return edgeList
+    let grouped = groupBy compareByQName $ filter (\x -> _mapq x > minMapQ) $ V.toList $ _alns paf 
+    let edgeList = concat $ map extractOverlapAlignment $ filter (\x -> (length x) >= 2) grouped 
+    edgeList
     --let (graph, nodeFromVertex, vertexFromKey) = graphFromEdges edgeList
     --return graph
 
+ctgPairToEdge :: (Contig, Contig) -> Edge
+ctgPairToEdge (a, b) = (contigAsInt a, contigAsInt b) 
+
 convertToEdge :: Aln -> Aln -> (Contig, Contig)
-convertToEdge a b = (convetToContig a, convertToContig b)
+convertToEdge a b = (convertToContig a, convertToContig b)
 
 convertToContig :: Aln -> Contig
 convertToContig a = case (_strand a) of
     '+' -> Contig (contigIdToInt $ _tname a) True
-    '-' -> Contig (contigIdToInt $_tname a) False
+    '-' -> Contig (contigIdToInt $ _tname a) False
 
-contigIdToInt :: T.Text -> int
+contigIdToInt :: T.Text -> Int
 contigIdToInt x = (myRead x) !! 0 
 
+-- https://stackoverflow.com/questions/28506961/how-to-parse-text-and-extract-integer
 myRead :: T.Text -> [Int]
 myRead = map num                 -- Convert the remaining elements into Ints
        . filter (not . T.null)   -- Drop all empty words
        . map (T.filter isDigit)  -- Drop all non-digits in each word (including signs!)
        . T.words                 -- Chop the string into words
+
+num :: T.Text -> Int 
+num = either (error . show) fst  -- Throw an exception if it wasn't a signed decimal
+    . signed decimal             -- Read a signed decimal
 
 --Author: Mark D. Blackwell
 --Change dates:
